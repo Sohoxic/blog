5f4f9d6d39e279199ea19ff832e6347874eabba8
SOHAM SARKAR <sarkarsoham73@gmail.com>
2025-02-12 12:42:02 +0530

    Update blogs.ts

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -4,7 +4,7 @@ export const blogPosts: BlogPost[] = [
   {
     id: '1',
     title: 'toxic',
-    description: 'Welcome to toxic by Sohoxic, a simple, fast and lightweight static site generator that transforms your markdown content into beautifully styled websites with minimal effort.',
+    description: 'toxic by Sohoxic is a simple, fast and lightweight static site generator that transforms your markdown content into beautifully styled websites with minimal effort.',
     path: '../../toxicBySohoxic.html',
     date: '2025-02-11',
     readingTime: '3 min read'
